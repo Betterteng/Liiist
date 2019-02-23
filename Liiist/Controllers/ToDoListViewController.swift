f258d86c9e86e98b8160135eba00e09deb6ec3de
Betterteng <betterteng@gmail.com>
2019-02-23 17:03:01 +1100

    Store data using CoreData complete...

--- Liiist/Controllers/ToDoListViewController.swift
+++ Liiist/Controllers/ToDoListViewController.swift
@@ -18,27 +18,33 @@ class ToDoListViewController: UITableViewController {
         }
     }
     
-    let content = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
     
-    // MARK - TableView data source methods
+    // MARK: - TableView data source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        //cell.textLabel?.text = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
         return cell
     }
 
-    // MARK - TableView delegate method
+    // MARK: - TableView delegate method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
@@ -48,23 +54,43 @@ class ToDoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    // MARK - Add new items...
+    // MARK: - Add new items...
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var localAlertTextField = UITextField()
+        
         let alert = UIAlertController(title: "Add new item ", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add", style: .default) {
-            (action) in
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
             // What will happen when user click the Add button
-            //self.itemArray.append(localAlertTextField.text!)
-            //self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+            let newItem = Item(context: self.context)
+            newItem.title = localAlertTextField.text!
+            newItem.done = false
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
         }
+        
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item..."
             localAlertTextField = alertTextField
         }
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Data manipulation methods
+    func saveItems() -> Void {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving data: \(error)")
+        }
+        
+        self.tableView.reloadData()
     }
     
 }
