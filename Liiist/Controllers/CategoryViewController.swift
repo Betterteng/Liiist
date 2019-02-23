a8d6d045204132f6139089f88e9576cac570a4ae
Betterteng <betterteng@gmail.com>
2019-02-23 19:56:31 +1100

    CategoryTableViewController has been added successfully.

---
+++ Liiist/Controllers/CategoryViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CategoryViewController.swift
+//  Liiist
+//
+//  Created by 滕施男 on 23/2/19.
+//  Copyright © 2019 BetterTeng. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        loadItems()
+    }
+    
+    // MARK: - Data manipulation methods
+    func saveItems() -> Void {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving data: \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Category> = Category.fetchRequest()) -> Void {
+        
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error data fetching from context: \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+    
+    // MARK: - TableView delegate method
+    
+    // MARK: - Add new entry method
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var localAlertTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new category ", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            // What will happen when user click the Add button
+            let newCategory = Category(context: self.context)
+            newCategory.name = localAlertTextField.text!
+            
+            self.categoryArray.append(newCategory)
+            
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category..."
+            localAlertTextField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
